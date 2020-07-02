279d211b2f59e39c2a0f7fb505430e090603f235
Jack Sanchez <evo@gms.tku.edu.tw>
2020-07-02 19:43:02 +0800

    .

--- notion_blog/settings.py
+++ notion_blog/settings.py
@@ -1,7 +1,8 @@
 
 
 import os
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
