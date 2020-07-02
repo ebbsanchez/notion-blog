67c6319d5e3206c797a027715c06e5f110bc4ef1
Jack Sanchez <evo@gms.tku.edu.tw>
2020-07-02 19:10:46 +0800

    .[

--- notion_blog/settings.py
+++ notion_blog/settings.py
@@ -136,9 +136,14 @@ USE_TZ = True
 
 
 # Path settings
-MEDIA_ROOT = PROJECT_PATH + '/media/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'production_static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
