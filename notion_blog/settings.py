1596ab13e1eb6849225c15db8ebb03793d101ceb
Jack Sanchez <evo@gms.tku.edu.tw>
2020-07-02 19:50:07 +0800

    .

--- notion_blog/settings.py
+++ notion_blog/settings.py
@@ -143,8 +143,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+print(BASE_DIR)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles'),
 )
