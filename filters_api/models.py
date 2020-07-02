63c73218b69ec3db7096853302e74f6de903be4e
Jack Sanchez <evo@gms.tku.edu.tw>
2020-07-02 17:17:59 +0800

    complete filters website functions and spark ar spider.

--- filters_api/models.py
+++ filters_api/models.py
@@ -5,9 +5,13 @@ from django_extensions.db.models import TimeStampedModel
 
 class Filter(TimeStampedModel):
     name = models.CharField(max_length=200)
-    demo = models.FileField(
-        upload_to='./videos/', null=True, verbose_name="")
+    # demo = models.FileField(
+        # upload_to='./videos/', null=True, verbose_name="")
 
+    demo = models.CharField(max_length=400)
     link = models.CharField(max_length=100)
     avatar = models.CharField(max_length=400)
-    slug = models.SlugField(unique=True)
+    # slug = models.SlugField(unique=True)
+
+    def __str__(self):
+    	return self.name
--- front_page/views.py
+++ front_page/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from filters_api.models import Filter
 # Create your views here.
+
+
 def index(request):
-	return render(request, 'front_page/index.html')
+	filters = Filter.objects.all()
+    # need refactor for api
+	context = {
+    	'h':filters[0],
+    	'hh':filters[1],
+    	'hhh':filters[2]
+    }
+	return render(request, 'front_page/index.html', context)
